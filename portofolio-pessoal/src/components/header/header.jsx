adc5b6fa5f1a694ce06e518ae7dc398baaaf02bb
Dev-LucasC <lucaconceicao@hotmail.com>
2023-02-23 14:40:30 -0300

    Deploy no vercel

--- portofolio-pessoal/src/components/header/header.jsx
+++ portofolio-pessoal/src/components/header/header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './header.css'
-
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 const Header = () => {
   return (
-    <div>heaaaaaaaaaaaaaaaaaaader</div>
+    <header>
+      <div className="container header__container">
+        <h5>Olá, eu sou </h5>
+        <h1>Lucas da Conceição</h1>
+        <h5 className="text-ligth">FullSatck Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+      </div>
+    </header>
   )
 }
 
