94101d285f8d1f7941f99f5054c47db365c9652d
Dev-LucasC <lucaconceicao@hotmail.com>
2023-02-23 17:17:09 -0300

    Deploy de continuação

--- portofolio-pessoal/src/components/header/header.jsx
+++ portofolio-pessoal/src/components/header/header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 const Header = () => {
   return (
     <header>
@@ -14,6 +16,8 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="me" />
         </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
       </div>
     </header>
   )
