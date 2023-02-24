e5ef944842c930ddcf5bce734cb7aea205cc69c9
Dev-LucasC <lucaconceicao@hotmail.com>
2023-02-24 11:01:12 -0300

    Seção de Serviços finalizada

--- portofolio-pessoal/src/components/services/Services.jsx
+++ portofolio-pessoal/src/components/services/Services.jsx
@@ -1,9 +1,121 @@
 import React from 'react'
 import './services.css'
+import { BiCheck } from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='services'>
+      <h5> O que posso oferecer</h5>
+      <h2>Serviços</h2>
+
+      <div className="container container__services">
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+          </ul>
+
+        </article>
+
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+          </ul>
+
+        </article>
+
+
+        <article className='service'>
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. icta! Eaque veniam ad veritatis fuga.</p>
+            </li>
+          </ul>
+
+        </article>
+
+      </div>
+
+
+
+    </section>
   )
 }
 
