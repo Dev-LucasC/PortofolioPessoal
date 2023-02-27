4804004647f78180d8317ae52184c84b43797dd9
Lucas da Conceição <111757758+Dev-LucasC@users.noreply.github.com>
2023-02-27 20:53:30 -0300

    Update HeaderSocials.jsx

--- portofolio-pessoal/src/components/header/HeaderSocials.jsx
+++ portofolio-pessoal/src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://linkedin.com/dev-lucasc" target='_blank'> <BsLinkedin /> </a>
+            <a href="https://www.linkedin.com/in/dev-lucasconceicao/" target='_blank'> <BsLinkedin /> </a>
             <a href="https://github.com/dev-lucasc" target='_blank'> <FaGithub /> </a>
             <a href="https://linkedin.com/dev-lucasc" target='_blank'></a>
         </div>
